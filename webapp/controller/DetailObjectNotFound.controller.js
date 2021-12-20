42e2b3d58b3f12d9b825599e44f008ebfea6c2fd
Vijay Singh <vijay.singh@spar.co.za>
2021-12-20 18:25:16 +0000

    main commit

---
+++ webapp/controller/DetailObjectNotFound.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+    "./BaseController"
+], function (BaseController) {
+    "use strict";
+
+    return BaseController.extend("za.co.spar.orders.display.splitscreenapp.controller.DetailObjectNotFound", {});
+});
